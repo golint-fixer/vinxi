fd5ea317a41a9addd9603fbedc12fcdbd9a33f75
Tomas Aparicio <tomas@aparicio.me>
2016-05-14 21:38:31 +0200

    feat(docs): add in-line API docs

--- manager/plugin.go
+++ manager/plugin.go
@@ -40,34 +40,45 @@ type plugin struct {
 	handler     Handler
 }
 
+// NewPlugin creates a new Plugin capable interface based on the
+// given HTTP handler logic encapsulated as plugin.
 func NewPlugin(name, description string, handler Handler) Plugin {
-	return &plugin{id: uniuri.New(), name: name, description: description, handler: handler}
+	return &plugin{id: uniuri.New(), sname: name, description: description, handler: handler}
 }
 
+// ID returns the plugin identifer.
 func (p *plugin) ID() string {
 	return p.id
 }
 
+// Name returns the plugin semantic name identifier.
 func (p *plugin) Name() string {
 	return p.name
 }
 
+// Description returns the plugin human readable description about
+// what the plugin does and for what it's designed.
 func (p *plugin) Description() string {
 	return p.description
 }
 
+// Disable disables the current plugin.
 func (p *plugin) Disable() {
 	p.disabled = true
 }
 
+// Enable enables the current plugin.
 func (p *plugin) Enable() {
 	p.disabled = false
 }
 
+// IsEnabled returns true if the plugin is enabled.
 func (p *plugin) IsEnabled() bool {
 	return p.disabled == false
 }
 
+// HandleHTTP implements the required plugin HTTP handler interface
+// triggered by the plugin layer during the incoming request call chain.
 func (p *plugin) HandleHTTP(h http.Handler) http.Handler {
 	next := p.handler(h)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -88,10 +99,12 @@ func NewPluginLayer() *PluginLayer {
 	return &PluginLayer{}
 }
 
-func (l *PluginLayer) Use(plugin Plugin) {
-	l.pool = append(l.pool, plugin)
+// Use registers one or multiples plugins in the current plugin layer.
+func (l *PluginLayer) Use(plugin ...Plugin) {
+	l.pool = append(l.pool, plugin...)
 }
 
+// Len returns the registered plugins length.
 func (l *PluginLayer) Len() int {
 	return len(l.pool)
 }
@@ -102,6 +115,8 @@ func (l *PluginLayer) Register(mw *layer.Layer) {
 	mw.Use("request", l.Run)
 }
 
+// Run triggers the plugins layer call chain.
+// This function is designed to be executed by top-level middleware layers.
 func (l *PluginLayer) Run(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	next := h
 	for _, plugin := range l.pool {
